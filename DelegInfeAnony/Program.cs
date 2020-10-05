c26dcd7eaa36eec6bf477dbede80d166898c4792
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-10-05 10:20:16 +0530

    Add project files.

---
+++ DelegInfeAnony/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DelegInfeAnony
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            //worker.WorkPerformed += Worker_WorkPerformed;
+            //worker.WorkCompleted += Worker_WorkCompleted;
+            //worker.WorkCompleted -= Worker_WorkCompleted;
+            
+            worker.WorkPerformed += delegate(object sender, WorkPerformedEventArgs e) 
+            {
+                Console.WriteLine("Hours worked : " + e.Hours + " " + e.WorkType);
+            };
+            worker.WorkCompleted += delegate (object sender, EventArgs ee)
+            {
+                Console.WriteLine("Work is done");
+            };
+
+            worker.DoWork(8, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        //static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Hours worked : " + e.Hours + " " + e.WorkType);
+        //}
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Work is done");
+
+        //}
+
+    }
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
+
---
+++ DelegInfeAnony/WorkPerformedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegInfeAnony
+{
+    public class WorkPerformedEventArgs : EventArgs
+    {
+        public WorkPerformedEventArgs(int hours, WorkType workType)
+        {
+            Hours = hours;
+            WorkType = workType;
+        }
+
+        public int Hours { get; set; }
+        public WorkType WorkType { get; set; }
+
+    }
+}
